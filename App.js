47cdfc6278f4b06ba7ec13a387355446521618e7
pooh <cktjdgus45@naver.com>
2022-09-29 21:20:44 +0900

    add first app-style

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+// <View></View>
+// <Text></Text>
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.wrapper}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>BongDam</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'tomato'
   },
+  weather: {
+    flex: 3,
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName: {
+    fontSize: 60,
+    fontWeight: '500'
+  },
+  temp: {
+    fontSize: 178,
+    marginTop: 50,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'teal',
+  }
 });
+
